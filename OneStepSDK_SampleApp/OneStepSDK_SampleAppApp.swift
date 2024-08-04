cc39a534d8969d6064cf63292cc0a61776d21f05
David Havkin <david@onestep.co>
2024-08-04 21:15:48 +0300

    sample app initial upload

---
+++ OneStepSDK_SampleApp/OneStepSDK_SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneStepSDK_SampleApp.swift
+//  OneStepSDK_SampleApp
+//
+//  Created by David Havkin on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct OneStepSDK_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
